edbef8b6d5e399a25f719962be9895e0d5c15603
RitikKumarSahoo <ritik@logic-square.com>
2025-04-22 17:20:29 +0530

    updated in index.js

--- index.js
+++ index.js
@@ -19,7 +19,51 @@ const stripeService = {
         reject(error)
       }
     })
-  }
+  },
+
+  createVendor(email, country = "US") {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const stripe = Stripe(this.getCurrentEnvironment());
+        const stripeResponse = await stripe.accounts.create({
+          email,
+          country,
+          type: "custom",
+          requested_capabilities: ["card_payments", "transfers"],
+          settings: {
+            payouts: {
+              schedule: {
+                interval: "manual",
+              },
+            },
+          },
+        });
+        resolve(stripeResponse);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
+
+  updateExternalAccountDefault(stripeId, accountId) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const stripe = Stripe(this.getCurrentEnvironment());
+        const stripeResponse = await stripe.accounts.updateExternalAccount(
+          stripeId,
+          accountId,
+          { default_for_currency: true }
+        );
+        resolve(stripeResponse);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
+
+  
+  
+
 }
 
 module.exports = stripeService
